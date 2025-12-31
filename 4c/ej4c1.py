bcfa354b94c6058b4ee7da4f02ed8b0806a5fdc8
laiajudeztallon-blip <laiajudeztallon@gmail.com>
2025-12-31 10:29:38 +0100

    Update ej4c1.py

--- 4c/ej4c1.py
+++ 4c/ej4c1.py
@@ -8,14 +8,30 @@ Se debe realizar los test en el archivo ej4c1_test.py considerando los distintos
 test.
 """
 
-def calculate_rectangle_area(width: int, height: int) -> int:
-    if not (isinstance(width, (int, float)) and isinstance(height, (int, float))):
-        raise TypeError("Width and height must be numbers.")
-    if width < 0 or height < 0:
-        raise ValueError("Width and height must be non-negative.")
+import pytest
+from ej4c1 import calculate_rectangle_area
+
+def test_calculate_rectangle_area():    
+    width = 3
+    height = 7
+    expected_area = 21
+    assert calculate_rectangle_area(width, height) == expected_area
     
-    area = width * height
-    return area
+def test_value_error_calculate_rectangle_area():
+    width = -2
+    height = 8
+    
+    with pytest.raises(ValueError):
+        calculate_rectangle_area(width, height)
+    
+
+def test_type_error_calculate_rectangle_area():
+    width = 'a'
+    height = 5
+    with pytest.raises(TypeError):
+        calculate_rectangle_area(width, height)
+
+    
 
 # Para probar el código, descomenta las siguientes líneas
 # if __name__ == "__main__":
